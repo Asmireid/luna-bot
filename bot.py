ee45c086e9c66b4341fb0368a6da2cd1eb3e7b7a
Asmireid <asmireid@hotmail.com>
2023-10-31 21:58:27 -0500

    Rename client to bot

--- bot.py
+++ bot.py
@@ -9,30 +9,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 async def update_status():
-    await client.change_presence(activity=discord.CustomActivity(name="Eating tacos. 🌮"))
+    await bot.change_presence(activity=discord.CustomActivity(name="Eating tacos. 🌮"))
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"Logged in as {client.user.name}.")
+    print(f"Logged in as {bot.user.name}.")
     await update_status()
 
 
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
-            await client.load_extension(f"cogs.{filename[:-3]}")
+            await bot.load_extension(f"cogs.{filename[:-3]}")
             # print(f"{filename[:-3]} is loaded.")
 
 
 async def main():
-    async with client:
+    async with bot:
         await load()
-        await client.start(os.getenv("BOT_TOKEN"))
+        await bot.start(os.getenv("BOT_TOKEN"))
 
 
 asyncio.run(main())
