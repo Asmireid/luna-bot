2bdbebee85a6977473f9ac2a0158e20d557df209
MacroSony <jameszh.wang@mail.utoronto.ca>
2023-11-04 02:06:26 -0400

    Enable chat with local Kobold api

---
+++ cogs/chat.py
@@ -0,0 +1,48 @@
+from utilities import *
+import requests
+import json
+
+
+url = 'http://localhost:5001/api/v1/generate'
+headers = {
+    'accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+# Define the request data as a Python dictionary
+request_data = {
+    "prompt": "",
+    "temperature": 0.5,
+    "top_p": 0.9
+}
+
+class Chat(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{os.path.basename(__file__)} is ready.")
+
+    @commands.command()
+    async def chat(self, ctx, *, question):
+        # Clean input
+        input = "###instruction: " + question
+        request_data['prompt'] = input
+        print(input)
+        # Convert the request data to JSON format
+        data = json.dumps(request_data)
+        # Send the POST request
+        response = requests.post(url, headers=headers, data=data)
+        # Check if the request was successful (HTTP status code 200)
+        # response_chat = ''
+        if response.status_code == 200:
+            # Parse and print the response as JSON
+            response_data = response.json()
+            text = response_data["results"][0]["text"]
+            print(text)
+        await try_reply(ctx, text)
+
+
+async def setup(bot):
+    await bot.add_cog(Chat(bot))
