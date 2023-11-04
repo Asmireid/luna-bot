d60a47157b062761448187e2010e7a40ac31b416
Asmireid <asmireid@hotmail.com>
2023-11-04 06:20:08 -0500

    Allow setting command_prefix when running

--- cogs/set_config.py
+++ cogs/set_config.py
@@ -9,13 +9,24 @@ def get_status():
     return config.bot_status
 
 
-def update_status(new_status):
-    config.configs.set('customizations', 'bot_status', new_status)
+async def set_helper(ctx, section, option, value: str):
+    config.configs.set(section, option, value)
     with codecs.open('config/config.ini', 'w', encoding='utf-8') as f:
         config.configs.write(f)
+    await try_display_confirmation(ctx, f"My new {option} is: {value}.")
 
 
-class SetConf(commands.Cog):
+async def command_prefix(ctx, value):
+    ctx.bot.command_prefix = value
+    await set_helper(ctx, 'settings', 'command_prefix', value)
+
+
+async def bot_status(ctx, value):
+    await ctx.bot.change_presence(activity=discord.CustomActivity(name=value))
+    await set_helper(ctx, 'customizations', 'bot_status', value)
+
+
+class SetConfig(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -24,19 +35,19 @@ class SetConf(commands.Cog):
         print(f"{os.path.basename(__file__)} is ready.")
 
     @commands.command()
-    async def set(self, ctx, conf, *, new_conf: str):
-        if conf == 'status':
-            await self.set_status(ctx, new_conf)
+    async def set(self, ctx, option, *, value: str):
+        switch_options = {
+            'status': bot_status,
+            'command_prefix': command_prefix
+        }
 
-    async def set_status(self, ctx, new_status: str):
-        update_status(new_status)
-        await self.bot.change_presence(activity=discord.CustomActivity(name=new_status))
-        if config.display_confirmation:
-            msg = f"My new status is: {new_status}."
-            conf = await try_reply(ctx, msg)
-            await try_delete_confirmation(conf)
-        await try_delete_invocation(ctx.message)
+        set_function = switch_options.get(option, None)
+
+        if set_function is not None:
+            await set_function(ctx, value)
+        else:
+            await try_display_confirmation(ctx, f"{option} is an invalid option...")
 
 
 async def setup(bot):
-    await bot.add_cog(SetConf(bot))
+    await bot.add_cog(SetConfig(bot))
