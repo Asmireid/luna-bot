a62e3928d5ad0cb4fdb966d4424f97b08de9b30a
Asmireid <asmireid@hotmail.com>
2023-11-10 06:00:01 -0600

    Add bin, oct, hex representations

--- cogs/calculator.py
+++ cogs/calculator.py
@@ -29,6 +29,30 @@ class Calc(commands.Cog):
         except:
             await try_reply(ctx, f"'{expression}' is not a valid expression...")
 
+    @commands.command(help="represents input integer in different representations: " +
+                           f"{Config().command_prefix}convert example_num")
+    async def represent(self, ctx, num):
+        try:
+            if '0b' in num:
+                int_num = int(num[2:], 2)
+            elif '0o' in num:
+                int_num = int(num[2:], 8)
+            elif '0x' in num:
+                int_num = int(num[2:], 16)
+            else:
+                int_num = int(num)
+
+            bin_num = bin(int_num)
+            oct_num = oct(int_num)
+            hex_num = hex(int_num)
+
+            msg_embed = make_embed(ctx,
+                                   title=f"{Config().bot_name}'s Calculator",
+                                   descr=f"{int_num} = {bin_num} = {oct_num} = {hex_num}")
+            await try_reply(ctx, msg_embed)
+        except:
+            await try_reply(ctx, f"'{num}' is not a valid representation of integer...")
+
 
 async def setup(bot):
     await bot.add_cog(Calc(bot))
