7aabc52b60896529fd6c89d89515864758401847
Asmireid <asmireid@hotmail.com>
2024-05-11 22:47:29 -0500

    bug fix

--- cogs/chat.py
+++ cogs/chat.py
@@ -7,14 +7,14 @@ from discord.ext import commands
 
 from utilities import *
 
-# Define the request data as a Python dictionary
+# define the request data as a Python dictionary
 request_data = {
     "prompt": "",
     "temperature": 0.5,
     "top_p": 0.9
 }
 
-# Save chat history globally
+# save chat history globally
 context = []
 
 # message queue
@@ -31,22 +31,21 @@ class Chat(commands.Cog):
 
     @commands.command(aliases=['说话'], help="chats with user")
     async def chat(self, ctx, *, message):
-            new_message = {'role': ctx.author.nick or ctx.author.name, 'content': message}
-            params = {'temperature': Config().temperature, 'top_p': Config().top_p, 'top_k': Config().top_k, 'max_new_tokens': Config().max_new_tokens}
-            context.append(new_message)
-            if len(context) > 20:
-                context.pop(0)
-            await chat_queue.put((context.copy(), params, ctx))
-            if chat_queue.qsize() == 1:
-                self.bot.loop.create_task(process_chat_queue())
+        new_message = {'role': ctx.author.nick or ctx.author.name, 'content': message}
+        params = {'temperature': Config().temperature, 'top_p': Config().top_p, 'top_k': Config().top_k,
+                  'max_new_tokens': Config().max_new_tokens}
+        await manage_context(new_message)
+        await chat_queue.put((context.copy(), params, ctx))
+        if chat_queue.qsize() == 1:
+            self.bot.loop.create_task(process_chat_queue())
 
     @commands.command(aliases=['清空', "忘记一切"], help="clears chat history")
     async def reset_chat(self, ctx):
         global context
         context = []
         await try_reply(ctx, "阿巴阿巴! 我忘记了一切!")
-            
-            
+
+
 async def manage_context(new_message):
     global context
     context.append(new_message)
@@ -65,14 +64,15 @@ async def process_chat_queue():
 
 async def chat_with_bot(curr_context, params, ctx):
     async with aiohttp.ClientSession() as session:
-        cleaned_sys_prompt = Config().system_prompt.replace("{{char}}",Config().bot_name).replace("{{user}}", ctx.author.nick or ctx.author.name)
+        cleaned_sys_prompt = Config().system_prompt.replace("{{char}}", Config().bot_name).replace("{{user}}",
+                                                                                                   ctx.author.nick or ctx.author.name)
         curr_context_with_system_prompt = [{'role': 'system', 'content': cleaned_sys_prompt}] + curr_context
         params['context'] = curr_context_with_system_prompt
         async with session.post(Config().api_url, json=params) as response:
             if response.status == 200:
                 response_data = await response.json()
                 response = {'role': Config().bot_name, 'content': response_data['response']}
-                await manage_context(response_data)
+                await manage_context(response)
                 await try_reply(ctx, response_data['response'])
             else:
                 raise Exception("Error fetching chat response. Status code: " + str(response.status))
